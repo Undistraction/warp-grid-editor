ab6d7952af987a43ceef0ce9d1a07fa9832aae5a
Pedr <pedr.browne@gmail.com>
2024-07-18 20:01:36 +0100

    chore: Use dotenv for vite config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 // eslint-disable-next-line
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // -----------------------------------------------------------------------------
 // Exports
 // -----------------------------------------------------------------------------
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
+export default defineConfig((mode) => {
+  const env = loadEnv(mode, process.cwd())
+  console.log('>>>>', mode, env)
+
+  return {
+    plugins: [react()],
+    css: {
+      postcss: {
+        plugins: [tailwindcss()],
+      },
     },
-  },
-  base: '/<REPO>/',
+    base: env.VITE_BASE || '/<REPO>/',
+  }
 })
