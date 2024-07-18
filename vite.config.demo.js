ec430cd295b3b476a1c2cd7496cde15b490ab098
Pedr <pedr.browne@gmail.com>
2024-07-18 20:26:18 +0100

    chore: Prepare to publish package

--- vite.config.demo.js
+++ vite.config.demo.js
@@ -1,14 +1,25 @@
+import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import tailwindcss from 'tailwindcss'
 // eslint-disable-next-line
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // -----------------------------------------------------------------------------
 // Exports
 // -----------------------------------------------------------------------------
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig((mode) => {
+  const env = loadEnv(mode, process.cwd())
+
   return {
+    plugins: [react()],
+    css: {
+      postcss: {
+        plugins: [tailwindcss()],
+      },
+    },
+    base: env.VITE_BASE || '/<REPO>/',
     build: {
       lib: {
         // Could also be a dictionary or array of multiple entry points
@@ -16,7 +27,6 @@ export default defineConfig(() => {
         name: 'coons-patch',
         // the proper extensions will be added
         fileName: 'coons-patch',
-        formats: ['es', 'cjs'],
       },
       rollupOptions: {
         // make sure to externalize deps that shouldn't be bundled
