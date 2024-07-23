6d561a6cdbd82a73231f31cf6f837ff1161ea7c5
Pedr <pedr.browne@gmail.com>
2024-07-23 16:19:46 +0100

    chore: Tweak semantic release config

--- release.config.js
+++ release.config.js
@@ -1,5 +1,10 @@
 export default {
-  release: {
-    branches: ['main'],
-  },
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git',
+    '@semantic-release/github',
+  ],
 }
