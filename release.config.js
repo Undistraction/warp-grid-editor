92c8f15af16a3e7dc201321d4d83bca6d9814b0f
Pedr <pedr.browne@gmail.com>
2024-07-23 16:02:34 +0100

    chore: Remove unused plugins

--- release.config.js
+++ release.config.js
@@ -2,15 +2,4 @@ export default {
   release: {
     branches: ['main'],
   },
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/github',
-      {
-        assets: ['dist/**'],
-      },
-    ],
-    '@semantic-release/git',
-  ],
 }
