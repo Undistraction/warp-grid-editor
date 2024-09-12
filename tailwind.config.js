f2cc379c1e48564fd11c9aa2963e290ed4a56924
Pedr <pedr.browne@gmail.com>
2024-09-12 16:13:16 +0100

    feat: Add custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 export default {
   content: [`./**/*.{js,jsx,html}`, `!./node_modules/`],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [`Nunito`, `ui-sans-serif`, `system-ui`, `sans-serif`],
+        serif: [`ui-serif`, `Georgia`, `serif`],
+        mono: [`ui-monospace`, `SFMono-Regular`, `monospace`],
+      },
+    },
   },
   plugins: [],
 }
